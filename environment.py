138c4a6a23cc905b08717bfb8e1d7ff3e2f3c72a
jamit <jamit@spiderorb.com>
2018-04-04 12:25:50 +0530

    adding driver path

--- environment.py
+++ environment.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 
 def before_all(context):
-    context.driver = webdriver.Firefox()
+    context.driver = webdriver.Firefox(executable_path ='/Users/jamit/Downloads/geckodriver')
     context.driver.implicitly_wait(30)
 def after_all(context):
     context.driver.quit()
