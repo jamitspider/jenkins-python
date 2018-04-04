480c77157a63acfac05eb9d08e40aee3c9b79eee
jamit <jamit@spiderorb.com>
2018-04-04 17:29:19 +0530

    final

--- environment.py
+++ environment.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
 
 def before_all(context):
-    context.driver = webdriver.Chrome()
+#     context.driver = webdriver.Firefox(executable_path='/Users/jamit/Downloads/driver/geckodriver')
+    context.driver = webdriver.Chrome(executable_path='/Users/jamit/Downloads/driver/chromedriver')
     context.driver.implicitly_wait(30)
 def after_all(context):
     context.driver.quit()
